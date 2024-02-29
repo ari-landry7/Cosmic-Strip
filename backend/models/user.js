df4a867a73f6c2240550fd747bb54a118a25add4
emma-landry7 <emmablandry730@gmail.com>
2024-02-28 21:43:38 -0600

    updated sign up form to connect to database

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: { type: String, trim: true, required: true, unique: true },
-    email: { type: String, trim: true, required: true, unique: true },
+    username: { type: String, trim: true,
+        // required: true,
+        unique: true },
+    email: { type: String, trim: true, 
+        // required: true, 
+        unique: true },
     password: { type: String },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }

ffdc678a00b1623811e43e4ff8474d8a59acd036
emma-landry7 <emmablandry730@gmail.com>
2024-02-28 18:54:59 -0600

    added protected routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,14 +4,15 @@ import HomePage from "../pages/HomePage"
 import PageNotFound from "../pages/PageNotFound"
 import GuestPage from "../pages/GuestPage"
 import CreatePost from "../pages/CreatePost"
+import ProtectedRoute from "./ProtectedRoute"
 
 function AppRoutes(props) {
     return (
         <Routes>
             <Route index element={<GuestPage {...props} />} />
-            <Route path='/home' element={<HomePage {...props} />} />
+            <Route path='/home' element={<ProtectedRoute><HomePage {...props} /></ProtectedRoute>} />
             <Route path='/login' element={<LoginForm {...props} />} />
-            <Route path='/new' element={<CreatePost {...props} />} />
+            <Route path='/new' element={<ProtectedRoute><CreatePost {...props} /></ProtectedRoute>} />
             <Route path='*' element={<PageNotFound />} />
         </Routes>
     )

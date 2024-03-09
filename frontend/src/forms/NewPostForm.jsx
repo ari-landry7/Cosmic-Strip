90c1aa8bd8c83c751ca1781da6e27b7e807646b6
emma-landry7 <emmablandry730@gmail.com>
2024-03-09 14:40:16 -0600

    useEffect was decalared twice

--- frontend/src/forms/NewPostForm.jsx
+++ frontend/src/forms/NewPostForm.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
-import { useEffect } from "react";
 import { Box, Button, TextField } from "@mui/material";
 
 function NewPostForm() {
